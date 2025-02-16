135913d942909becde8090559af32354f53a654b
Denis Tola <denis.tola22@gmail.com>
2025-02-16 16:38:26 +0100

    Update Shop Layout
    Adding first styling to shop layout

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,9 +1,18 @@
 import { Tabs } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>["name"];
+    color: string;
+}) {
+    return <FontAwesome size={24} style={{ marginBottom: -3, color: "#1BC464" }} {...props} />;
+}
 
 const TabsLayout = () => { 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={style.safeArea}>
             <Tabs
                 screenOptions={{
                     tabBarActiveTintColor: "blue",
@@ -17,13 +26,27 @@ const TabsLayout = () => {
                 }}
             >
                 <Tabs.Screen name="index"
-                options={{headerShown: true}} 
+                options={{
+                    title: "Shop",
+                    tabBarIcon(props) {
+                        return <TabBarIcon name="shopping-cart" color={props.color} />;
+                    }
+                }} 
                 />
                 <Tabs.Screen name="orders"
-                options={{}} 
+                options={{
+                    title: "Orders",
+                    tabBarIcon(props) {
+                        return <TabBarIcon name="shopping-cart" color={props.color} />;
+                    }
+                }} 
                 />
             </Tabs>
         </SafeAreaView>
     ); 
 }
 export default TabsLayout;
+
+const style = StyleSheet.create({ 
+    safeArea: {flex: 1,}
+ });
